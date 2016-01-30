9983e879142b0e575d983d397ba0dac1a3d11e6c
ajbertra91 <ajbertra91@gmail.com>
2016-01-30 07:42:27 -0500

    updates falseyBouncer

--- bonfires/falseyBouncer.js
+++ bonfires/falseyBouncer.js
@@ -1,17 +1,27 @@
+// function bouncer(arr) {
+//   // Don't show a false ID to this bouncer.
+//   return arr.filter(function(item) {
+//     switch(item) {
+//       case 0:
+//       case "":
+//       case undefined:
+//       case NaN:
+//       case 0:
+//       case false:
+//         return item;
+//       default:
+//         return item;
+//     }
+//   });
+// }
+
+// bouncer([7, "ate", "", false, 9]);
+
+
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
-  return arr.filter(function(item) {
-    switch(item) {
-      case 0:
-      case "":
-      case undefined:
-      case NaN:
-      case 0:
-      case false:
-        return item;
-      default:
-        return item;
-    }
+  return arr.filter(function(item) { 
+    return item; 
   });
 }
 
