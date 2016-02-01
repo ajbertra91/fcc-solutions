85e54e4329d203d30a7f18310417b419ff1710df
Adam <adam@sportstechinc.com>
2016-02-01 10:46:43 -0500

    saves a file

--- bonfires/falseyBouncer.js
+++ bonfires/falseyBouncer.js
@@ -1,19 +1,19 @@
-// function bouncer(arr) {
-//   // Don't show a false ID to this bouncer.
-//   return arr.filter(function(item) {
-//     switch(item) {
-//       case 0:
-//       case "":
-//       case undefined:
-//       case NaN:
-//       case 0:
-//       case false:
-//         return item;
-//       default:
-//         return item;
-//     }
-//   });
-// }
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  return arr.filter(function(item) {
+    switch(item) {
+      case 0:
+      case "":
+      case undefined:
+      case NaN:
+      case 0:
+      case false:
+        return item;
+      default:
+        return item;
+    }
+  });
+}
 
 // bouncer([7, "ate", "", false, 9]);
 
